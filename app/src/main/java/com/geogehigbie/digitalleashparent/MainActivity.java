bbdd5d0da4be320bdc6f4a3f28589a6b9193d159
George Higbie <gehigbie@gmail.com>
2016-10-15 20:09:14 -0400

    small changes made

--- app/src/main/java/com/geogehigbie/digitalleashparent/MainActivity.java
+++ app/src/main/java/com/geogehigbie/digitalleashparent/MainActivity.java
@@ -1,11 +1,20 @@
 package com.geogehigbie.digitalleashparent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     private String latitude;
 
     private JSONObject parentJSON;
+    private String JSONString;
+
+    private String URLString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         getChildStatus();
 
-        try {
-            createJSON();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+
     }
 
     public void getParentData(){
@@ -43,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         longitude = parentLongitute.getText().toString();
         latitude = parentLatitude.getText().toString();
 
+        createJSON();
+
     }
 
 
@@ -50,15 +62,89 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void createJSON() throws JSONException {
-        JSONObject parentJSON = new JSONObject();
-        parentJSON.put("userID", userID);
-        parentJSON.put("radius", radius);
-        parentJSON.put("longitute", longitude);
-        parentJSON.put("latitude", latitude);
 
+    public void createJSON(){
+        parentJSON = new JSONObject();
+
+        try{
+            parentJSON.put("userID", userID);
+            parentJSON.put("radius", radius);
+            parentJSON.put("longitute", longitude);
+            parentJSON.put("latitude", latitude);
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
 
     }
 
+
     //need to do HTTP request activity on seperate thread with an Asynchronous request
+
+    //makes use of OKHTTP to post
+
+
+    public void HTTPRequestRun(String userID, String URL, String JSON){
+        this.userID = userID;
+
+        URLString = "https://turntotech.firebaseio.com/digitalleash/" + "userID" + ".json.";
+        OkHttpClient client = new OkHttpClient();
+
+        String post(String url, String json) throws IOException {
+            RequestBody body = RequestBody.create(JSON, json);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        }
+
+//        public class PostExample {
+//
+//            OkHttpClient client = new OkHttpClient();
+//
+//            String post(String url, String json) throws IOException {
+//                RequestBody body = RequestBody.create(JSON, json);
+//                Request request = new Request.Builder()
+//                        .url(url)
+//                        .post(body)
+//                        .build();
+//                try (Response response = client.newCall(request).execute()) {
+//                    return response.body().string();
+//                }
+//            }
+//
+//
+//        }
+
+    public void senddatatoserver(View v) {
+
+        URLString = "https://turntotech.firebaseio.com/digitalleash/" + "userID" + ".json.";
+
+        if (parentJSON.length() > 0) {
+            new SendJsonDataToServer().execute(String.valueOf(parentJSON));
+            #call to async class
+        }
+
+
+
+    }
+// TODO: AsyncTask
+
+    class SendDataToServer extends AsyncTask<String,String,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+        }
+
+    }
+}
+
 }
