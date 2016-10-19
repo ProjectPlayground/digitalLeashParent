ff203a2d4db7a8983b5f4403fd7dc684babbc419
George Higbie <gehigbie@gmail.com>
2016-10-19 19:13:51 -0400

    network request is working

--- app/src/main/java/com/geogehigbie/digitalleashparent/MainActivity.java
+++ app/src/main/java/com/geogehigbie/digitalleashparent/MainActivity.java
@@ -6,28 +6,19 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Random;
 
 import layout.FragmentBadChild;
 import layout.FragmentDataParent;
 import layout.FragmentGoodChild;
-import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -47,14 +38,10 @@ public class MainActivity extends FragmentActivity {
 
     private String URLString;
 
-    private RelativeLayout relativeLayout;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
 
     private boolean isGoodChild;
-
-    //HTTP variables
-    private OkHttpClient client = new OkHttpClient();   //from OkHttp
 
 
     @Override
@@ -65,12 +52,13 @@ public class MainActivity extends FragmentActivity {
 
         loadFirstFragment();
 
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-        getActionBar().hide();
+//        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+//        getActionBar().hide();
 
     }
 
-    public void loadFirstFragment(){
+    public void loadFirstFragment() {
+
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.fragment_container, new FragmentDataParent());
@@ -79,37 +67,42 @@ public class MainActivity extends FragmentActivity {
     }
 
 
-    public void onClickCreate(View view){
+    public void onClickCreate(View view) {
 
         playSoundEffects();
         getParentData();
 
-        setContentView(view); //sends data to server
+        sendJSONData();
+
+        setTextViews();
 
     }
 
 
-    public void onClickCheckStatus(View view){
+    public void onClickCheckStatus(View view) {
 
         playSoundEffects();
         getChildStatus();
 
-        if(isGoodChild){
+        if (isGoodChild) {
             loadGoodChildFragment();
 
-        }
-        else{
+        } else {
             loadBadChildFragment();
 
         }
 
+        setTextViews();
+
     }
 
 
-    public void onClickUpdate(View view){
+    public void onClickUpdate(View view) {
 
         playSoundEffects();
+        setTextViews();
         getParentData();
+        sendJSONData();
 
     }
 
@@ -122,76 +115,120 @@ public class MainActivity extends FragmentActivity {
     }
 
 
+    public void onClickBadChild(View view) {
+        String warningMessage;
 
-    public void onClickBadChild(View view){
-        String warningMessage = "Your kid is playing in traffic! Panic!!!";
+        Random random = new Random();
+        int number = random.nextInt(3);
+
+        if (number == 0) {
+            warningMessage = "Your kid is playing in traffic and smoking crack! Panic!!!";
+        } else if (number == 1) {
+            warningMessage = "Your kid is robbing a bank. You failed as a parent! Panic!!!";
+        } else {
+            warningMessage = "The cops have your child. You are truly a terrible parent. Panic!!!";
+        }
 
         playSoundEffects();
         messageToast(warningMessage);
-        loadFirstFragment();
+        getSupportFragmentManager().popBackStack();
+
 
     }
 
-    public void onClickGoodChild(View view){
-        String partyMessage = "Your kid is alive. Get Drunk!";
+    public void onClickGoodChild(View view) {
+
+        String partyMessage;
+
+        Random random = new Random();
+        int number = random.nextInt(3);
+
+        if (number == 0) {
+            partyMessage = "Your kid is alive but has ADHD. Get Drunk!";
+        } else if (number == 1) {
+            partyMessage = "Your kid is NOT robbing a bank. Get Drunk!";
+        } else {
+            partyMessage = "Your kid is NOT smoking crack. Get Drunk!";
+        }
+
 
         playSoundEffects();
         messageToast(partyMessage);
-        loadFirstFragment();
+        // loadFirstFragment();
+        getSupportFragmentManager().popBackStack();
     }
 
 
-    public void playSoundEffects(){
+    public void playSoundEffects() {
 
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.click_on_sound);
         mediaPlayer.start();
 
     }
 
+
     public void getParentData() {
         EditText parentUserID = (EditText) findViewById(R.id.usernameEditText);
         EditText parentRadius = (EditText) findViewById(R.id.radiusEditText);
-        EditText parentLongitute = (EditText) findViewById(R.id.longitudeEditText);
+        EditText parentLongitude = (EditText) findViewById(R.id.longitudeEditText);
         EditText parentLatitude = (EditText) findViewById(R.id.latitudeEditText);
 
         userIDParent = parentUserID.getText().toString();
         radius = parentRadius.getText().toString();
-        longitude = parentLongitute.getText().toString();
+        longitude = parentLongitude.getText().toString();
         latitude = parentLatitude.getText().toString();
 
         createJSON();
 
     }
 
+    public void setTextViews() {
+        EditText parentUserID = (EditText) findViewById(R.id.usernameEditText);
+        EditText parentRadius = (EditText) findViewById(R.id.radiusEditText);
+        EditText parentLongitude = (EditText) findViewById(R.id.longitudeEditText);
+        EditText parentLatitude = (EditText) findViewById(R.id.latitudeEditText);
+
+        userIDParent = parentUserID.getText().toString();
+        radius = parentRadius.getText().toString();
+        longitude = parentLongitude.getText().toString();
+        latitude = parentLatitude.getText().toString();
+
+        parentUserID.setText(userIDParent);
+        parentRadius.setText(radius);
+        parentLongitude.setText(longitude);
+        parentLatitude.setText(latitude);
+    }
+
 
     public void getChildStatus() {
 
         Random random = new Random();
-        int number =  random.nextInt(2);
+        int number = random.nextInt(2);
 
 
-        if(number == 0){
+        if (number == 0) {
             isGoodChild = false;
-        }
-        else{
+        } else {
             isGoodChild = true;
         }
 
     }
 
 
-    public void loadBadChildFragment(){
+    public void loadBadChildFragment() {
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, new FragmentBadChild());
+        fragmentTransaction.add(R.id.fragment_container, new FragmentBadChild()).addToBackStack(null);//.commit();
         fragmentTransaction.commit();
 
     }
 
-    public void loadGoodChildFragment(){
+
+    public void loadGoodChildFragment() {
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, new FragmentGoodChild());
+        fragmentTransaction.add(R.id.fragment_container, new FragmentGoodChild());
+        fragmentTransaction.addToBackStack(null);//.commit();
         fragmentTransaction.commit();
 
     }
@@ -201,197 +238,231 @@ public class MainActivity extends FragmentActivity {
         parentJSON = new JSONObject();
 
         try {
-            parentJSON.put("userID", userIDParent);
+            parentJSON.put("username", userIDParent);
             parentJSON.put("radius", radius);
-            parentJSON.put("longitute", longitude);
+            parentJSON.put("longitude", longitude);
             parentJSON.put("latitude", latitude);
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-       JSONString = parentJSON.toString();
+        JSONString = parentJSON.toString();
         return JSONString;
 
     }
 
-    public String createURL(String userID){
+    public String createURL(String userID) {
         String URLBase = "https://turntotech.firebaseio.com/digitalleash/";
-        userID = userID + ".json.";
+        userID = userID + ".json";
 
         URLString = URLBase + userID;
         return URLString;
     }
 
-    public void sendData(){
-        String URLString = createURL(userIDParent);
+
+    public class SendJSONData extends AsyncTask<Void, Void, String> {
+
         String JSONString = createJSON();
+        String URLString = createURL(userIDParent);
 
-        new AsyncTask<Void, Void, String>(){
+        @Override
+        protected String doInBackground(Void... params) {
 
-            @Override
-            protected String doInBackground(Void... params) {
-                return getServerResponse();
+            Log.d("doInBackground", "called");
 
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-            }
-        }
-    }
-
-    private String getServerResponse(){
-        return null;
-    }
-
-    public void basicURLConnection(){
-
-        createURL(userIDParent);
-        HttpURLConnection connection = null;
-        BufferedReader reader = null;
-
-        try {
-            URL url = new URL(URLString);
-            connection = (HttpURLConnection) url.openConnection();
-
-            InputStream stream = connection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(stream));
-            StringBuffer buffer = new StringBuffer();
-
-            String line= "";
-
-            while((line = reader.readLine()) != null){
-
-            }
-
-
-        }catch(MalformedURLException exception){
-            exception.printStackTrace();
-        }catch(IOException exception){
-            exception.printStackTrace();
-        }finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
             try {
-                if(reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                Log.d("try", "called");
 
-   }
-
-
-    public void senddatatoserver(View v) {
-
-        //calls the createJSONFunction
-        String JSONString = createJSON();
-        String URLString = createURL(userIDParent);
-
-        class SendDataToServer extends AsyncTask<String,String,String> {
-
-            @Override
-            protected String doInBackground(String... params) {
-
-                //creates a new OKHttpClient
                 OkHttpClient client = new OkHttpClient();
 
-                //defines a request being sent to server
-                RequestBody postData = new FormBody.Builder()
-                        .add("type", "json")
+                RequestBody postdata = RequestBody.create(JSON, JSONString);
+
+                Request request = new Request.Builder()
+                        .url(URLString)
+                        .addHeader("X-HTTP-Method-Override","PUT")
+                        .post(postdata)
                         .build();
 
-                //
-                Request request = new Request.Builder()
-                        .url(URLString);
-                .post(postData);
-                .build();
-
-                //Execute the request
                 Response response = client.newCall(request).execute();
+
+                if (response.isSuccessful()) {
+                    Log.d("response", "isSuccessful");
+                } else {
+                    Log.d("response", response.message());
+
+                }
+
                 String result = response.body().string();
                 return result;
 
-            }
-
-            catch(
-            Exception e
-            )
-
-            {
+            } catch (Exception e) {
+                Log.d("Exception", e.getMessage());
                 return null;
             }
         }
 
-
-
-
-
-            @Override
-            protected void onPostExecute(String s) {
-            }
-
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
         }
     }
+
+    public void sendJSONData() {
+
+        JSONString = createJSON();
+        URLString = createURL(userIDParent);
+
+        SendJSONData sendJSONData = new SendJSONData();
+        sendJSONData.execute();
+
+    }
+}
+
+
+//    public void basicURLConnection(){
+//
+//        createURL(userIDParent);
+//        HttpURLConnection connection = null;
+//        BufferedReader reader = null;
+//
+//        try {
+//            URL url = new URL(URLString);
+//            connection = (HttpURLConnection) url.openConnection();
+//
+//            InputStream stream = connection.getInputStream();
+//            reader = new BufferedReader(new InputStreamReader(stream));
+//            StringBuffer buffer = new StringBuffer();
+//
+//            String line= "";
+//
+//            while((line = reader.readLine()) != null){
+//
+//            }
+//
+//
+//        }catch(MalformedURLException exception){
+//            exception.printStackTrace();
+//        }catch(IOException exception){
+//            exception.printStackTrace();
+//        }finally {
+//            if (connection != null) {
+//                connection.disconnect();
+//            }
+//            try {
+//                if(reader != null) {
+//                    reader.close();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//   }
+
+
+//    public void senddatatoserver(View v) {
+//
+//        //calls the createJSONFunction
+//        String JSONString = createJSON();
+//        String URLString = createURL(userIDParent);
+//
+//        class SendDataToServer extends AsyncTask<String,String,String> {
+//
+//            @Override
+//            protected String doInBackground(String... params) {
+//
+//                //creates a new OKHttpClient
+//                OkHttpClient client = new OkHttpClient();
+//
+//                //defines a request being sent to server
+//                RequestBody postData = new FormBody.Builder()
+//                        .add("type", "json")
+//                        .build();
+//
+//                //
+//                Request request = new Request.Builder()
+//                        .url(URLString);
+//                .post(postData);
+//                .build();
+//
+//                //Execute the request
+//                Response response = client.newCall(request).execute();
+//                String result = response.body().string();
+//                return result;
+//
+//            }
+//
+//            catch(
+//            Exception e
+//            )
+//
+//            {
+//                return null;
+//            }
+//        }
+//
+//
+//
+//
+//
+//            @Override
+//            protected void onPostExecute(String s) {
+//            }
+//
+//        }
+//    }
 
 //add background inline class here
-        class SendJsonDataToServer extends Async<String,String,String>{
-        }
+//        class SendJsonDataToServer extends Async<String,String,String>{
+//
+//
+//        }
 
 
+//need to do HTTP request activity on seperate thread with an Asynchronous request
+
+//makes use of OKHTTP to post
+
+//    public String post(String url, String json) throws IOException {
+//        RequestBody body = RequestBody.create(JSON, json);
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .post(body)
+//                .build();
+//        Response response = client.newCall(request).execute();
+//
+//        return response.body().string();
+//    }
 
 
-    //need to do HTTP request activity on seperate thread with an Asynchronous request
-
-    //makes use of OKHTTP to post
-
-    public String post(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(JSON, json);
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
-        Response response = client.newCall(request).execute();
-
-        return response.body().string();
-    }
+//    public void HTTPRequestRun(String userID, String URL, String JSON) {
+//        userID = userIDParent;
+//
+//        URLString = "https://turntotech.firebaseio.com/digitalleash/" + "userID" + ".json.";
+//
+//        createJSON();
+//        post(URLString, JSONString);
+//
+//    }
 
 
-    public void HTTPRequestRun(String userID, String URL, String JSON) {
-        userID = userIDParent;
-
-        URLString = "https://turntotech.firebaseio.com/digitalleash/" + "userID" + ".json.";
-
-        createJSON();
-        post(URLString, JSONString);
-
-    }
-
-
-
-
-
-        public class PostExample {
-
-            OkHttpClient client = new OkHttpClient();
-
-            String post(String url, String json) throws IOException {
-                RequestBody body = RequestBody.create(JSON, json);
-                Request request = new Request.Builder()
-                        .url(url)
-                        .post(body)
-                        .build();
-                try (Response response = client.newCall(request).execute()) {
-                    return response.body().string();
-                }
-            }
-
-
-        }
+//        public class PostExample {
+//
+//            OkHttpClient client = new OkHttpClient();
+//
+//            String post(String url, String json) throws IOException {
+//                RequestBody body = RequestBody.create(JSON, json);
+//                Request request = new Request.Builder()
+//                        .url(url)
+//                        .post(body)
+//                        .build();
+//                try (Response response = client.newCall(request).execute()) {
+//                    return response.body().string();
+//                }
+//            }
+//
+//
+//        }
 
 //    public void senddatatoserver(View v) {
 //
@@ -403,7 +474,6 @@ public class MainActivity extends FragmentActivity {
 //        }
 
 
-    }
 // TODO: AsyncTask
 
 //    class SendDataToServer extends AsyncTask<String,String,String> {
@@ -422,4 +492,4 @@ public class MainActivity extends FragmentActivity {
 //}
 //
 //}
-}
+
